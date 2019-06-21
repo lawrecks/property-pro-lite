abc9d53e2f9edc2e5e77f8b1b174da07e7ba7bd1
lawrecks <ugobugo2011@gmail.com>
2019-06-21 21:57:39 +0100

    feat(updatePropertyAd): create update property advert page
    - create update property advert page in the property pro application
    
    [Delivers #166853148]

--- UI/js/app.js
+++ UI/js/app.js
@@ -107,3 +107,62 @@ const changeName = () => {
   let upload_button = document.getElementById("upBtn");
   upload_button.innerHTML = new_image;
 };
+
+const openEditModal = () => {
+  let modal = document.getElementById("edit_modal");
+  let close_button = document.getElementsByClassName("close")[2];
+  let cancel_button = document.getElementById("cancel_edit");
+  let save_button = document.getElementById("save_edit");
+  modal.style.display = "block";
+  close_button.onclick = () => {
+    modal.style.display = "none";
+  };
+  cancel_button.onclick = () => {
+    modal.style.display = "none";
+  };
+  save_button.onclick = () => {
+    modal.style.display = "none";
+  };
+  window.onclick = event => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+};
+
+const openDeleteModal = () => {
+  let modal = document.getElementById("delete_modal");
+  let close_button = document.getElementById("close_delete");
+  let cancel_button = document.getElementById("cancel_delete");
+  let delete_button = document.getElementById("do_delete");
+  modal.style.display = "block";
+  close_button.onclick = () => {
+    modal.style.display = "none";
+  };
+  cancel_button.onclick = () => {
+    modal.style.display = "none";
+  };
+  delete_button.onclick = () => {
+    modal.style.display = "none";
+  };
+  window.onclick = event => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+};
+
+const markAsSold = id => {
+  let btns = document.getElementsByClassName("mark-button");
+  for (let btn of btns) {
+    if (btn.id.substring(4) == id && btn.innerHTML == "Mark as sold") {
+      btn.style.background = "#099200";
+      btn.innerHTML = "Sold!";
+      break;
+    } else if (btn.id.substring(4) == id && btn.innerHTML == "Sold!") {
+      btn.style.background = "#022024";
+      btn.innerHTML = "Mark as sold";
+      break;
+    }
+  }
+};
